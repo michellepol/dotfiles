23613b9f93edcbaefb2e33462fb49e9ba40686fa
Michelle <michellepol33@gmail.com>
2025-01-13 20:29:43 +0300

    stow directories

--- setup.sh
+++ setup.sh
@@ -17,19 +17,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # install powerlevel10k theme for oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# make symbolic links to repo
-#
-#TODO: Add links to ~/.zshrc and ~/.p10k
-files=$(ls $PWD)
-
-for file in $files;
-do
-    if [[ -d $file ]]; then
-        target="$PWD/$file"
-        linkname="$HOME/.config/$file"
-
-        echo "make symlink $linkname -> $target"
-
-        ln -s $target $linkname
-    fi
-done
+stow -R -v -t ~ .
