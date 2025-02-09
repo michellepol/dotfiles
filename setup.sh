7039231d569c67a49a9e994d2ee77e58a5a9fa05
michellepol <michellepol33@gmail.com>
2025-02-09 16:58:03 +0300

    hyprland config

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 # install zsh
 
-echo "install zsh & change default shell"
+echo "install dependencies & change default shell to zsh"
 
-sudo pacman -S zsh
+sudo pacman -S zsh stow nodejs npm
 
 # change shell to zsh
 
@@ -17,6 +17,18 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # install powerlevel10k theme for oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-sudo pacman -S stow
+echo "link config to ~/"
 
 stow -R -v -t ~ .
+
+# neovim
+
+# Install packer
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# Install nodejs (for coc and gitsigns)
+sudo pacman -S 
+
+nvim -c 'PackerInstall'
