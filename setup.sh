6b8226e2932ec211804116aa9156f5cb56bdb393
michellepol <michellepol33@gmail.com>
2025-02-10 22:17:52 +0300

    micro changes to hypr

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# install packages
+echo "install packages"
+sudo pacman -S kitty bitwarden waybar hyprland hyprpaper wofi
+
 # install zsh
 
 echo "install dependencies & change default shell to zsh"
 
 sudo pacman -S zsh stow nodejs npm
 
-# change shell to zsh
+## change shell to zsh
 
 chsh -s /bin/zsh
 
@@ -14,13 +18,17 @@ echo "install oh-my-zsh framework"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# install powerlevel10k theme for oh-my-zsh
+## install powerlevel10k theme for oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/mafredri/zsh-async.git ${ZSH_CUSTOM}/plugins/zsh-async
 
+# make links to repo
 echo "link config to ~/"
 
 stow -R -v -t ~ .
+
+# fonts
+echo "install fonts"
 
 # neovim
 
@@ -30,6 +38,6 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # Install nodejs (for coc and gitsigns)
-sudo pacman -S 
+sudo pacman -S nodejs npm
 
 nvim -c 'PackerInstall'
